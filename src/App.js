96cec861bd00861fd7a17279cf842793b4f5ddc6
juancho.villa@icloud.com <juancho.villa@icloud.com>
2020-11-08 19:21:15 -0700

    Add the chatbot functionality to the repo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
+import  React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [entries, setEntries] = useState(["Hello, there."]);
+  const [entry, setEntry] = useState('');
+  const [reply, setReply] = useState(entries[Math.floor(Math.random() * entries.length)]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="Output">
+        <p>{reply}</p>
+      </div>
+
+      {/*
+        I plan on adding React-Bootstrap to make the UI prettier.
+      */}
+
+      <div className="Input">
+        <form>
+          <input 
+            placeholder="Chat with me..."
+            name="entry" value={entry}
+            onChange={(e) => setEntry(e.target.value)}>
+          </input>
+          <button 
+            onPointerDown={() => {
+              setEntries([...entries, entry])
+              setReply(entries[Math.floor(Math.random() * entries.length)])
+            }}
+            type="button" >
+              Enter
+            </button>
+        </form>
+      </div>
     </div>
   );
 }
--- src/setupTests.js
+++ src/setupTests.js
@@ -1,5 +1,3 @@
-// jest-dom adds custom jest matchers for asserting on DOM nodes.
-// allows you to do things like:
-// expect(element).toHaveTextContent(/react/i)
-// learn more: https://github.com/testing-library/jest-dom
+// I'll be using jest-dom for testing.
+// At least that's what `create-react-app` belives I should do.
 import '@testing-library/jest-dom';
