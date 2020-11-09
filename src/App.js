51a4ccb8354d76043bae4e50882d0fa5f1b718bf
juancho.villa@icloud.com <juancho.villa@icloud.com>
2020-11-08 20:37:15 -0700

    Add a basic chat log

--- src/App.js
+++ src/App.js
@@ -1,22 +1,44 @@
 import  React, { useState } from 'react';
 import './App.css';
 
+function sample(array) {
+  if ( array.length === 1 ) {
+    return array[0]
+  } else if ( array.length > 1) {
+    return array[Math.floor(Math.random() * array.length)]
+  }
+
+  return null;
+}
+
 function App() {
-  const [entries, setEntries] = useState(["Hello, there."]);
+  const [entries, setEntries] = useState([]);
   const [entry, setEntry] = useState('');
-  const [reply, setReply] = useState(entries[Math.floor(Math.random() * entries.length)]);
+  // eslint-disable-next-line
+  const [replies, setReplies] = useState([])
+  const [reply, setReply] = useState(sample(entries));
+
+  let displayText = (
+    <p>Start chatting by typing a message.</p>
+  )
+
+  if ( reply ) {
+    displayText = (
+      <p>{ reply }</p>
+    )
+  }
 
   return (
     <div className="App">
       <div className="Output">
-        <p>{reply}</p>
+        <h1>Transcript</h1>
+        <p>{replies.length > 0 ? replies[replies.length - 1] : "..."}</p>
+        <p>{entries.length > 0 ? entries[entries.length - 1] : "..."}</p>
       </div>
 
-      {/*
-        I plan on adding React-Bootstrap to make the UI prettier.
-      */}
-
       <div className="Input">
+        { displayText }
+
         <form>
           <input 
             placeholder="Chat with me..."
@@ -26,7 +48,9 @@ function App() {
           <button 
             onPointerDown={() => {
               setEntries([...entries, entry])
-              setReply(entries[Math.floor(Math.random() * entries.length)])
+              setEntry("")
+              setReply(sample(entries))
+              setReplies([...replies, reply])
             }}
             type="button" >
               Enter
